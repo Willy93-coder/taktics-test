0811400caaf573d5e082b75be5711cef37ed7e8a
Guillermo Cifre <guillecg13@gmail.com>
2025-04-02 21:14:37 +0200

    Added operations logic

--- api/common/models/budget.js
+++ api/common/models/budget.js
@@ -1,5 +1,29 @@
-'use strict';
-
 module.exports = function(Budget) {
+  Budget.prototype.recalculateTotals = function(cb) {
+    const budget = this;
+    budget.chapters(function(err, chapters) {
+      if (err) return cb(err);
+      let totalCost = 0;
+      let totalSale = 0;
+      chapters.forEach(chapter => {
+        totalCost += chapter.totalCostImport;
+        totalSale += chapter.totalSaleImport;
+      });
+      Budget.updateAll({ id: budget.id }, {
+        totalCostImport: totalCost,
+        totalSaleImport: totalSale
+      }, cb);
+    });
+  };
 
+  Budget.observe('after save', function(ctx, next) {
+    let instance = ctx.instance;
+    if (!instance) {
+      return next();
+    } 
+
+    instance.recalculateTotals(function(err) {
+      next(err);
+    });
+  });
 };
